2373d4269740800ba3f44b387c3f49aeca7178c0
Saurav Singh <sausingh9@publicisgroupe.net>
2021-04-25 19:37:20 +0530

    Committing changed

--- src/test/java/com/cucumber/demo/CucumberRunTests.java
+++ src/test/java/com/cucumber/demo/CucumberRunTests.java
@@ -2,11 +2,14 @@ package com.cucumber.demo;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/resources/features", glue={"resources/stepDefs"}, monochrome=true)
-public class TestRunner {
+public class CucumberRunTests {
 
 
 
